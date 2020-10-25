544527320acb8bb8adb7900d0abd1b373e2c5087
Martín Stancanelli <mastanca14@gmail.com>
2020-10-25 19:16:41 -0300

    fix mapstream

--- map_reduce/map_reduce.go
+++ map_reduce/map_reduce.go
@@ -15,3 +15,23 @@ func Reduce(list []int, fn func(int, int) int) int {
 	}
 	return result
 }
+
+func MapStream(list []string, fn func(string) int) <-chan int {
+	result := make(chan int, len(list))
+	go func() {
+		defer close(result)
+		for _, s := range list {
+			result <- fn(s)
+		}
+	}()
+
+	return result
+}
+
+func ReduceStream(ints <-chan int, fn func(int, int) int) int {
+	var result int
+	for n := range ints {
+		result = fn(result, n)
+	}
+	return result
+}
--- map_reduce/map_reduce_test.go
+++ map_reduce/map_reduce_test.go
@@ -1,8 +1,12 @@
 package map_reduce
 
 import (
-	"github.com/stretchr/testify/assert"
+	"sort"
 	"testing"
+
+	go_concurrent_utils "github.com/mastanca/go-concurrent-utils"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMap(t *testing.T) {
@@ -17,21 +21,59 @@ func TestMap(t *testing.T) {
 
 func TestReduce(t *testing.T) {
 	difference := func(a, b int) int {
-		return a-b
+		return a - b
 	}
 	ints := []int{1, 2, 3, 4}
 	result := Reduce(ints, difference)
 	assert.Equal(t, -10, result)
 }
 
-func TestMapReduce(t *testing.T)  {
+func TestMapReduce(t *testing.T) {
 	strings := []string{"a", "ab", "abc", "abcd"}
 	count := func(s string) int {
 		return len(s)
 	}
 	sum := func(a, b int) int {
-		return a+b
+		return a + b
 	}
 	result := Reduce(Map(strings, count), sum)
+	assert.Equal(t, 10, result)
+}
+
+func TestMapStream(t *testing.T) {
+	strings := []string{"a", "ab", "abc", "abcd"}
+	count := func(s string) int {
+		return len(s)
+	}
+
+	out := MapStream(strings, count)
+	var result []int
+	for range strings {
+		result = append(result, <-out)
+	}
+	sort.Ints(result)
+	assert.Equal(t, []int{1, 2, 3, 4}, result)
+}
+
+func TestReduceStream(t *testing.T) {
+	difference := func(a, b int) int {
+		return a - b
+	}
+	ints := []int{1, 2, 3, 4}
+	done := make(chan interface{})
+	defer close(done)
+	result := ReduceStream(go_concurrent_utils.IntGenerator(done, ints...), difference)
+	assert.Equal(t, -10, result)
+}
+
+func TestMapReduceStream(t *testing.T) {
+	strings := []string{"a", "ab", "abc", "abcd"}
+	count := func(s string) int {
+		return len(s)
+	}
+	sum := func(a, b int) int {
+		return a + b
+	}
+	result := ReduceStream(MapStream(strings, count), sum)
 	assert.Equal(t, 10, result)
 }
